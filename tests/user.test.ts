71b55dfb6a0c4ffea32b0969c54d8ecaecbb5166
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-12-26 00:13:23 +0530

    failed test case for chcking  empty name in user intialse throw error

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -4,6 +4,10 @@ describe("User Class",()=>{
     test("Should successfully create a valid user instance", () => {
             const user = new User("Deep", User.Role.LIBRARIAN);
             expect(user).toBeDefined();
-            
+          });
+          test("Should throw exception when Name is null or empty", () => {
+            expect(() => new User("", User.Role.LIBRARIAN)).toThrow(
+              "ISBN should not be null or empty"
+            );
           });
 })
