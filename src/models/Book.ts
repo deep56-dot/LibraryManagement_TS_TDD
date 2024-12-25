dd0cc6cb340a7b1fd32744aea3bcdeee18745e78
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-12-25 23:39:02 +0530

    pass test case for initalise book functinlaity added

--- src/models/Book.ts
+++ src/models/Book.ts
@@ -1,4 +1,15 @@
 
 export class Book {
-  
+  constructor(
+    private isbn: string,
+    private title: string,
+    private author: string,
+    private publicationYear: Number
+  ) {
+    
+  }
+
+  getISBN(): string {
+    return this.isbn;
+  }
 }
--- tests/book.test.ts
+++ tests/book.test.ts
@@ -2,8 +2,8 @@ import {Book} from "../src/models/Book"
 
 describe("Book Class", () => {
     test("Should successfully create a valid book instance", () => {
-        const book = new Book("9780143414969", "The Guide", "R.K. Narayan", Year.of(1958));
+        const book = new Book("9780143414969", "The Guide", "R.K. Narayan", 1958);
         expect(book).toBeDefined();
-        
+        expect(book.getISBN()).toBe("9780143414969");
       });
   });
