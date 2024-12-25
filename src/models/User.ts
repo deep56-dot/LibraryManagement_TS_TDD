81a8c1d3a46ab127e562e8f4bb5cbf6d6a79ec4a
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-12-26 00:14:09 +0530

    passed  test case for chcking  empty name in user intialse throw error

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ export class User {
     static Role = { LIBRARIAN: "LIBRARIAN", USER: "USER" };
   
     constructor(private name: string, private role: string) {
-     
+      if (!name || name.trim() === "") throw new Error("Name should not be null or empty");
     }
   
     
