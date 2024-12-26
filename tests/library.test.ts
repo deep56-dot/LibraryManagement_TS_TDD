5ae0b2e2445d55abac0bf229bee1c75ffa840109
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-12-26 12:31:08 +0530

    failed test case for Should throw exception when returning a book not borrowed by user

--- tests/library.test.ts
+++ tests/library.test.ts
@@ -96,6 +96,23 @@ describe("Library Class Tests", () => {
     expect(library["borrowedBooks"].get("9788172234980")).toBeUndefined();
   });
 
+  test("Should throw exception when returning a book not borrowed by user", () => {
+    const librarian = new User("Deep", User.Role.LIBRARIAN);
+    const user1 = new User("Heta", User.Role.USER);
+    const user2 = new User("Parth", User.Role.USER);
+    const book = new Book("9788172234980", "The White Tiger", "Aravind Adiga", 2008);
+
+    library.addUser(librarian);
+    library.addUser(user1);
+    library.addUser(user2);
+    library.addBook(librarian, book);
+
+    library.borrowBook(user1, "9788172234980");
+    expect(() => library.returnBook(user2, "9788172234980")).toThrow(
+      "Book was not borrowed by this user"
+    );
+  });
+
 
 
   
